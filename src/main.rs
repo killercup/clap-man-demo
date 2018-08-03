c2795d0595ed4ad930d1d4ee49a4fe45d0c880de
Pascal Hertleif <killercup@gmail.com>
2018-08-03 18:56:35 +0200

    One more fixme

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use clap::{IntoApp, FromArgMatches};
 mod cli;
 
 fn main() -> Result<(), Box<Error>> {
+    // FIXME: This should be `cli::Head::parse()`
     let args = cli::Head::from_argmatches(&cli::Head::into_app().get_matches());
     std::fs::read_to_string(&args.file)?
         .lines()
